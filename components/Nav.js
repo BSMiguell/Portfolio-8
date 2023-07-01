9e1c6607a0cc8f692e0e2b0a71c8d6bb2410c99f
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-30 23:42:28 -0300

    Fist Commit

---
+++ components/Nav.js
@@ -0,0 +1,32 @@
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from "react-icons/hi";
+
+//  links
+export const links = [
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
+  {
+    name: "testimonials",
+    path: "/testimonials",
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: "contact",
+    path: "/contact",
+    icon: <HiEnvelope />,
+  },
+];
+
+const Nav = () => {
+  return <nav>nav</nav>;
+};
+
+export default Nav;
