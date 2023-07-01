1b9a5b2ad0c1014a4b6c8674cc9fbdf7e6d5bb7e
BSMiguell <bruno.s.miiguel@gmail.com>
2023-07-01 10:21:12 -0300

    teste

--- pages/services/index.js
+++ pages/services/index.js
@@ -11,6 +11,7 @@ const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
+
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
