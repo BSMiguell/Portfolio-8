08efd780ff0b43ca90f5886292f4188f610b3379
BSMiguell <bruno.s.miiguel@gmail.com>
2023-07-01 04:24:13 -0300

    Justando o index.js

--- pages/index.js
+++ pages/index.js
@@ -60,9 +60,16 @@ const Home = () => {
       </div>
 
       {/* image */}
-      <div>Image</div>
-      {/* bg img */}
-      <div></div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg img */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+
+        {/* porticles */}
+        <div>porticles</div>
+        <div>
+          <Avatar />
+        </div>
+      </div>
     </div>
   );
 };
