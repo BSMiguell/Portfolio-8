c6d12386b15357da19a9e7889623c1e88aba981c
BSMiguell <bruno.s.miiguel@gmail.com>
2023-07-01 04:10:53 -0300

    finalizando o ProjectsBtn.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Avatar from "../components/Avatar";
 import { motion } from "framer-motion";
 
 // variants
-import { tadeln } from "../variants";
+import { fadeIn, tadeln } from "../variants";
 
 const Home = () => {
   return (
@@ -19,21 +19,43 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* Title */}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            inherit="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Transforming Ideas <br /> Into
             <span className="text-accent">Digital Reality</span>
-          </h1>
+          </motion.h1>
 
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
             nostrum quam reprehenderit vero, tenetur voluptatem nulla aut
             aspernatur dolores ut.
-          </p>
+          </motion.p>
 
           {/* Btn */}
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBth />
           </div>
+
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBth />
+          </motion.div>
         </div>
       </div>
 
